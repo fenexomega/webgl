741b592e3d1fa884e8a9a68d8d9401d357edb813
Jordy <jordyfgomes@gmail.com>
2015-07-22 14:59:19 -0300

    cleaning code

--- drawing/triangle.js
+++ drawing/triangle.js
@@ -1,144 +1,9 @@
 var color = vec3(0.5,0.5,0.5)
 var maxLines = 2000000
 var gl;
-var points;
 var programNbr;
-var numTriangles = 1;
-var vertices 
-var angle = 0.0;//1.54;
-var maxSubdivisions;
-var triangleSize = 0.5;
+var vertices = []
 var canvas;
-var figure = 0;
-var wireFrame = false;
-var gasket = false;
-
-
-function distance(p1,p2)
-{
-  return Math.sqrt(Math.pow((p2[0] - p1[0]),2) + Math.pow((p2[1] - p1[1]),2));
-};
-
-function subtractArray(arr1,arr2)
-{
-  if(arr1.length != arr2.length)
-    throw "Diferent Lenght of arrays!";
-  var result = [];
-  for(i = 0; i < arr1.length; ++i)
-  {
-    result.push(arr1[i] - arr2[i]);
-  }
-  return result;
-};
-
-function sumArray(arr1,arr2)
-{
-  if(arr1.length != arr2.length)
-    throw "Diferent Lenght of arrays!";
-  var result = [];
-  for(i = 0; i < arr1.length; ++i)
-  {
-    result.push(arr1[i] + arr2[i]);
-  }
-  return result;
-};
-
-function divideArray(arr,nbr)
-{
-  var aux = [];
-  for(i of arr)
-  {
-    aux.push(i/nbr);
-  }
-  return aux;
-};
-
-function getVectorFromTo(p1,p2)
-{
-	var result = [];
-	for(i = 0 ; i < p1.length; ++i)
-	{
-		result.push(p2[i] - p1[i]);
-	}
-	return result;
-};
-
-function twist(p,center)
-{
-	var x = p[0];
-	var  y = p[1];
-	d = distance(p,center);
-	x = x*Math.cos(d*angle) - y*Math.sin(d*angle);
-	y = x*Math.sin(d*angle) + y*Math.cos(d*angle);
-	return [x,y];
-}
-
-function middleBetween(p1,p2)
-{
-	var result = [(p1[0]+p2[0])/2,(p1[1]+p2[1])/2];
-	return result;
-};
-
-function addTriangle(p1,p2,p3)
-{
-	vertices.push(p1);
-	vertices.push(p2);
-	vertices.push(p3);
-};
-
-function addSquare(p1,p2,p3,p4)
-{
-	addTriangle(p1,p2,p3);
-	addTriangle(p1,p4,p3);
-}
-
-function setCenter(c)
-{
-	var ucenter = gl.getUniformLocation(programNbr,"center"); 
-	gl.uniform2f(ucenter,c[0],c[1]);
-
-}
-
-function subdivideTriangle(p1,p2,p3,n)
-{
-  if(n ==  0)
-  {
-    addTriangle(p1,p2,p3);
-    return;
-  }
-  var m12 = middleBetween(p1,p2);
-  var m23 = middleBetween(p2,p3);
-  var m31 = middleBetween(p3,p1);
-  n -= 1;
-  subdivideTriangle(m12,p2,m23,n);
-  subdivideTriangle(p1,m12,m31,n);
-  if(!gasket)
-	  subdivideTriangle(m12,m23,m31,n);
-  subdivideTriangle(m31,m23,p3,n);
-};
-
-function renderTriangles(n)
-{
-
-	vertices = []
-	var vertice =[[-0.5,-0.35],[0.5,-0.35],[0.0,0.65]]
-
-	maxSubdivisions = n;
-    //  Configure WebGL
-
-
-    // Load the data into the GPU
-    var bufferId = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData( gl.ARRAY_BUFFER, maxLines*8, gl.STATIC_DRAW );
-
-    // Associate out shader variables with our data buffer
-    var vPosition = gl.getAttribLocation( programNbr, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
-
-    render();
-};
 
 
 window.onload = function init()
@@ -152,8 +17,15 @@ window.onload = function init()
     gl.useProgram( program );
 	programNbr = program;
     gl.viewport( 0, 0, canvas.width, canvas.height );
-	canDraw = false
-	renderTriangles(0);
+    // Load the data into the GPU
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, maxLines*8, gl.STATIC_DRAW );
+
+    // Associate out shader variables with our data buffer
+    var vPosition = gl.getAttribLocation( programNbr, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
 	canvas.addEventListener("mousedown",function(event){
 		canDraw = true
 	})
@@ -170,6 +42,7 @@ window.onload = function init()
 		gl.bufferSubData(gl.ARRAY_BUFFER,(vertices.length-1)*8,flatten(t))
 		render()
 	})
+	canDraw = false
 };
 
 
