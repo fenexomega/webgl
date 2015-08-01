e918e556aefcb1e1d3d3221a78b35ecf237d11f2
Jordy <jordyfgomes@gmail.com>
2015-08-01 09:15:36 -0300

    assignment 3

---
+++ assignment_3/assignment3.js
@@ -0,0 +1,138 @@
+var backgroundColor = vec3(0.1,0.1,0.1)
+var maxLines = 2000000
+var gl;
+var programNbr;
+var vertices = []
+var canvas;
+var sliders 
+var clCanvasCtx
+var lineWidth = 1
+
+function initUI()
+{
+}
+
+function changeLineWidth(num)
+{
+	lineWidth = num
+	gl.lineWidth(lineWidth)
+	render()
+}
+
+function canvasClear()
+{
+	vertices = []
+	canDraw = false
+	render()
+}
+
+function getSliders()
+{
+	sliders = []
+	sliders.push(document.getElementById("slidR"))
+	sliders.push(document.getElementById("slidG"))
+	sliders.push(document.getElementById("slidB"))
+	sliders.push(document.getElementById("slidLineWidth"))
+	clCanvasCtx = document.getElementById("color-canvas").getContext('2d')
+}
+
+function getHexString(number)
+{
+	var string = number.toString(16)
+	while(string.length < 6)
+		string = '0' + string
+	return string
+}
+
+function changeColor()
+{
+	var value = 0
+	color = vec3(sliders[0].value/255,
+				 sliders[1].value/255,
+				 sliders[2].value/255)
+	for(var i = 0; i < 3; ++i)
+		value += Number(sliders[i].value) << 8*(2-i)
+	var colorString = "#" + getHexString(value)
+	console.log(colorString)
+	clCanvasCtx.fillStyle = colorString
+	clCanvasCtx.fillRect(0,0,64,64)
+
+}
+
+function getGLMousePosition(event)
+{
+	return [2*(event.clientX-canvas.offsetLeft)/canvas.width-1,
+			1-(2*(event.clientY-canvas.offsetTop)/canvas.height)]
+}
+
+function addVertexFromMouse(event)
+{
+	var t = getGLMousePosition(event)
+	vertices.push(t);
+	gl.bindBuffer(gl.ARRAY_BUFFER,bufferId)
+	gl.bufferSubData(gl.ARRAY_BUFFER,(vertices.length-1)*8,flatten(t))
+	gl.bindBuffer(gl.ARRAY_BUFFER,colorBufferId)
+	gl.bufferSubData(gl.ARRAY_BUFFER,(vertices.length-1)*12,flatten(color))
+}
+
+window.onload = function init()
+{
+    canvas = document.getElementById( "gl-canvas" );
+	getSliders()
+	initUI()
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+    //  Load shaders and initialize attribute buffers
+    program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+	gl.lineWidth(3)
+	
+    // Load the data into the GPU
+    bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, maxLines*8, gl.STATIC_DRAW );
+
+	colorBufferId = gl.createBuffer()
+	gl.bindBuffer(gl.ARRAY_BUFFER, colorBufferId)
+	gl.bufferData(gl.ARRAY_BUFFER, maxLines*12, gl.STATIC_DRAW)
+
+	canvasClear()
+    // Associate out shader variables with our data buffer
+	gl.bindBuffer(gl.ARRAY_BUFFER,bufferId)
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+	gl.bindBuffer(gl.ARRAY_BUFFER,colorBufferId)
+	var	vColor = gl.getAttribLocation( program, "vColor")
+	gl.vertexAttribPointer( vColor, 3, gl.FLOAT, false, 0,0);
+	gl.enableVertexAttribArray(vColor)
+	
+	canvas.addEventListener("mousedown",function(event){
+		if(!canDraw)
+		{
+		}
+		canDraw = true
+	})
+	
+	canvas.addEventListener("mouseup",function(event){
+		if(canDraw)
+		{
+		}
+		canDraw = false
+	})
+	canvas.addEventListener("mousemove",function(event){
+
+	})
+	canDraw = false
+	render()
+};
+
+
+
+function render() {
+    gl.clearColor( backgroundColor[0], backgroundColor[1], backgroundColor[2], 1.0 );
+    gl.clear( gl.COLOR_BUFFER_BIT );
+	gl.drawArrays( gl.LINES, 0, vertices.length);
+}
