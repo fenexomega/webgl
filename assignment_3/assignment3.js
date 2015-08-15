c571ccbbe92b48c0bef800bf01914ced640d192d
Jordy <jordyfgomes@gmail.com>
2015-08-15 12:40:46 -0300

    udpate

--- assignment_3/assignment3.js
+++ assignment_3/assignment3.js
@@ -90,7 +90,7 @@ function resetGUI()
 	for (var i = 0, len = rotation_sliders.length; i < len; i++) {
 		rotation_sliders[i].value = 0;
 	}
-	scale_slider.value = 0
+	scale_slider.value = 1
 }
 
 function initGUI()
@@ -101,6 +101,7 @@ function initGUI()
 	document.getElementById("distance").value = 0
 	document.getElementById("cbxWireframe").checked = false
 	document.getElementById("div_geochoose").childNodes[3].checked = true
+	document.getElementById("html5colorpicker").value = "#00FFFF"
 	scale_slider = document.getElementById("scaletotal")
 }
 
