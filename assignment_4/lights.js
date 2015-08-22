987d275b2f8c5729e57f6471c2405e9bddc6e6c3
Jordy <jordyfgomes@gmail.com>
2015-08-22 09:43:55 -0300

    update

---
+++ assignment_4/lights.js
@@ -0,0 +1,13 @@
+
+var lights = []
+
+function createLight(v3_dif,v3_spec,v3_amb)
+{
+	//TODO get :wq
+	var obj
+	// TODO create light object	
+	
+}
+
+
+
---
+++ assignment_4/materials.js
@@ -0,0 +1,4 @@
+function createMaterial(v3amb, v3dif, v3spe, shininess)
+{
+	return { ambient: v3amb, diffuse: v3dif, specular: v3spe, shininess: shininess }		
+}
