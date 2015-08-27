a60ac461a3eb2b91f6e25f952bd70ba6dc40320e
Jordy <jordyfgomes@gmail.com>
2015-08-27 18:44:31 -0300

    update

--- assignment_4/materials.js
+++ assignment_4/materials.js
@@ -1,9 +1,24 @@
 function createMaterial(v3amb, v3dif, v3spe, shininess)
 {
-	function setOnShader()
-	{
-			
-	}
 
-	return { ambient: v3amb, diffuse: v3dif, specular: v3spe, shininess: shininess }		
+	var obj = { 
+		ambient: v3amb,
+		difuse: v3dif,
+		specular: v3spe,
+		shininess: shininess,
+		sendToShader: function ()	{
+			var ambient = gl.getUniformLocation(program,"Material.ambient")		
+			var difuse = gl.getUniformLocation(program,"Material.difuse")		
+			var specular = gl.getUniformLocation(program,"Material.specular")		
+			var shininess = gl.getUniformLocation(program,"Material.shininess")		
+
+			gl.Uniform3f(ambient,this.ambient[0],this.ambient[1],this.ambient[2])
+			gl.Uniform3f(difuse,this.difuse[0],this.difuse[1],this.difuse[2])
+			gl.Uniform3f(specular,this.specular[0],this.specular[1],this.specular[2])
+			gl.Uniform1f(shininess,this.shininess)
+
+		}
+	}		
+
+	return obj
 }
